30c5fe23a4a9cb43b64cd7f9cd71f5d0913fe8c5
bharathibhuvana24 <bharathibhuvana24@gmail.com>
2024-07-30 13:29:51 +0530

    created a test api

--- API/index.js
+++ API/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js';
 
 dotenv.config();
 
@@ -11,7 +12,11 @@ mongoose
 {
     console.log('Connected to MongoDB');
 }
-)
+).catch((err)=>
+{
+    console.log(err);
+}
+);
 
 
 const app = express();
@@ -22,3 +27,5 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000!!!');
 }
 );
+
+app.use('/api/user', userRouter)
